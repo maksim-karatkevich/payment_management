3f16282cfd3a90d741d29ad59dd1cbd9774bf2b3
Maksim <maksim_karatkevich@epam.com>
2017-11-15 16:55:33 +0300

    model added

---
+++ src/main/java/model/Event.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Created by Maksim_Karatkevich on 11/15/2017.
+ */
+@Entity
+@Table(name = "event")
+public class Event {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name = "date")
+	private Date date;
+	@Column(name = "sum")
+	private double sum;
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+	@ManyToOne
+	@JoinColumn(name = "shop_id")
+	private Shop shop;
+
+	public Event(long id, Date date, double sum, Category category, Shop shop) {
+		this(id, date, sum, shop);
+		this.category = category;
+	}
+
+	public Event(long id, Date date, double sum, Shop shop) {
+		this.id = id;
+		this.date = date;
+		this.sum = sum;
+		this.shop = shop;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getSum() {
+		return sum;
+	}
+
+	public void setSum(double sum) {
+		this.sum = sum;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Shop getShop() {
+		return shop;
+	}
+
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+
+	@Override
+	public String toString() {
+		return "Event{" +
+				"id=" + id +
+				", date=" + date +
+				", sum=" + sum +
+				", category=" + category +
+				", shop=" + shop +
+				'}';
+	}
+}
---
+++ src/main/java/model/Shop.java
@@ -0,0 +1,51 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by Maksim_Karatkevich on 11/15/2017.
+ */
+@Entity
+@Table(name = "shop")
+public class Shop {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name = "name")
+	private String name;
+
+	public Shop(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Shop{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
